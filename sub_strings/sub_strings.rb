ed4572fef889ac031825099cdefa976ae967f309
Samuel Hafner <hafner@pixelpoint.at>
2024-01-05 09:51:49 +0100

    extend substring method to handle multiple words input

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,15 +1,18 @@
-def substrings(word, dictionary)
-    word = word.downcase
-    results = Hash.new(0)
-    dictionary.each do |string|
-        string = string.downcase
-        if word.include?(string)
-            results[string] += 1
-        end
+def substrings(words, dictionary)
+  arr_words = words.split
+  results = Hash.new(0)
+  dictionary.each do |string|
+    string = string.downcase
+    arr_words.each do |word|
+      if word.include?(string)
+        results[string] += 1
+      end
     end
-    results
+  end
+  results
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
