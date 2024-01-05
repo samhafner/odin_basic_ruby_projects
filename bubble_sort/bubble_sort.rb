5010755bdf7e5ab94e1d3d8c93377443e6e98731
Samuel Hafner <hafner@pixelpoint.at>
2024-01-05 11:28:52 +0100

    improve bubble_sort: break if no swap took place, get rid of temp var

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,14 +1,20 @@
 def bubble_sort(arr)
   n = arr.length
-  (n - 1).times do
-    for i in (0..(n-2))
+
+  loop do
+    swapped = false
+
+    # (n-1).times will go from 0 to (n-1)-1, so we always stay within the array
+    (n - 1).times do |i|
       if arr[i] > arr[i + 1]
-        temp = arr[i + 1]
-        arr[i + 1] = arr[i]
-        arr[i] = temp
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
       end
     end
+
+    break unless swapped
   end
+
   arr
 end
 
